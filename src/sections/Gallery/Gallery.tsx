6a84b6058fa16a2a39cd1e30ae84b0d3347cc335
Violeta <vnaboishchykova@gmail.com>
2024-04-30 00:26:31 +0200

    base_of_Gallery

--- src/sections/Gallery/Gallery.tsx
+++ src/sections/Gallery/Gallery.tsx
@@ -1,10 +1,36 @@
+import Image from "next/image";
 export default function Gallery() {
   return (
     <section
       id="gallery"
       className="bg-gallery bg-cover bg-center bg-no-repeat h-lvh w-full wrapper"
     >
-      <div className="container"></div>
+      <div className="container">
+        <h2>
+          Our
+          <span>Gallery</span>
+        </h2>
+        <div>
+          <Image
+            src="/img/services/slide-1.jpg"
+            width={280}
+            height={187}
+            alt="Slide"
+          />
+          <Image
+            src="/img/services/slide-2.jpg"
+            width={280}
+            height={187}
+            alt="Slide"
+          />
+          <Image
+            src="/img/services/slide-3.jpg"
+            width={280}
+            height={187}
+            alt="Slide"
+          />
+        </div>
+      </div>
     </section>
   );
 }
